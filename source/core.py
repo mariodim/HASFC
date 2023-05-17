8742658c0b885fc88271dfa3f63184e2cf727174
Giovanni.Galatro <giovanni.galatro@ibm.com>
2023-05-17 14:14:43 +0200

    Format autopep8

--- source/core.py
+++ source/core.py
@@ -6,110 +6,137 @@ import csv
 import itertools
 import time
 
+import numpy as np
+
 from source.cache import *
 
 DEFINITION = "\nDEFINITION_PARAMETER "
 POSITION = " 9.41 2.07"
 
+
 def async_operation(id, configuration, parameters):
     run_timenet(parameters, id)
     elaboration(configuration, id)
 
-def run_timenet(parameters,id):
+
+def run_timenet(parameters, id):
     try:
-        max_VNF = min( parameters.pop('maxVNF'), 6)
-        max_NR = min( parameters.pop('maxNR'), 4)
-    
-        header_values_to_timenet=""
+        max_VNF = min(parameters.pop('maxVNF'), 6)
+        max_NR = min(parameters.pop('maxNR'), 4)
+
+        header_values_to_timenet = ""
         for key in parameters:
-            if isinstance(parameters[key],float) or isinstance(parameters[key],int):
+            if isinstance(
+                    parameters[key],
+                    float) or isinstance(
+                    parameters[key],
+                    int):
                 header_values_to_timenet = f'{header_values_to_timenet}{DEFINITION}{key} {parameters[key]}{POSITION}'
 
         counter = 0
         values = set()
-        for subset in itertools.combinations_with_replacement( [k for k in range(max_VNF, -1, -1)], int(max_NR)):
+        for subset in itertools.combinations_with_replacement(
+                [k for k in range(max_VNF, -1, -1)], int(max_NR)):
             values.add(subset)
 
         # Init log file
         log_file = open(f"logs//{id}_log", "a+")
         log_file.write(f"{counter}|{len(values)*2}\n")
         log_file.close()
-        
+
         # Init output file
-        output_file = open(f"{id}.csv","w+")
+        output_file = open(f"{id}.csv", "w+")
         output_file.write("VNF1;VNF2;VNF3;VNF4;perf;parallelo\n")
-        for perf in range(3,5):
+        for perf in range(3, 5):
             for value in values:
-                VNF1=value[0]             
-                VNF2=0 if len(value) < 2 else value[1]   
-                VNF3=0 if len(value) < 3 else value[2]   
-                VNF4=0 if len(value) < 4 else value[3]  
+                VNF1 = value[0]
+                VNF2 = 0 if len(value) < 2 else value[1]
+                VNF3 = 0 if len(value) < 3 else value[2]
+                VNF4 = 0 if len(value) < 4 else value[3]
                 counter += 1
-                if VNF1+VNF2+VNF3+VNF4 > perf: 
-                    timenet_values = f'{header_values_to_timenet} {DEFINITION} VNF1 {VNF1}{POSITION}{DEFINITION} VNF2 {VNF2}{POSITION}{DEFINITION} VNF3 {VNF3}{POSITION}{DEFINITION} VNF4 {VNF4}{POSITION}\n'     
-                        
-                    input_file_to_timenet = open(f"homogeneous\\data_{id}",'w')
+                if VNF1 + VNF2 + VNF3 + VNF4 > perf:
+                    timenet_values = f'{header_values_to_timenet} {DEFINITION} VNF1 {VNF1}{POSITION}{DEFINITION} VNF2 {VNF2}{POSITION}{DEFINITION} VNF3 {VNF3}{POSITION}{DEFINITION} VNF4 {VNF4}{POSITION}\n'
+
+                    input_file_to_timenet = open(
+                        f"homogeneous\\data_{id}", 'w')
                     input_file_to_timenet.write(timenet_values)
                     input_file_to_timenet.close()
-                    
+
                     try:
                         compose_timenet_file(id)
-                                    
+
                         subprocess.check_output(f"rm -rf {id}.dir")
-                        subprocess.run('"C:\\Program Files (x86)\\TimeNET\\TimeNET\\EDSPN\\StatAnalysis\\scripts\\SOLVE.bat" '+ id + ' -E -s -i 10000 1e-10', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                        
-                        result = read_result(id)                
-                        output_file.write(f"{VNF1};{VNF2};{VNF3};{VNF4};{perf};{result}\n")
-                    except:
+                        subprocess.run(
+                            '"C:\\Program Files (x86)\\TimeNET\\TimeNET\\EDSPN\\StatAnalysis\\scripts\\SOLVE.bat" ' +
+                            id +
+                            ' -E -s -i 10000 1e-10',
+                            stdout=subprocess.DEVNULL,
+                            stderr=subprocess.DEVNULL)
+
+                        result = read_result(id)
+                        output_file.write(
+                            f"{VNF1};{VNF2};{VNF3};{VNF4};{perf};{result}\n")
+                    except BaseException:
                         time.sleep(1)
                         compose_timenet_file(id)
-                        
-                        subprocess.check_output('"C:\\Program Files (x86)\\TimeNET\\TimeNET\\EDSPN\\StatAnalysis\\scripts\\SOLVE.bat"'+' '+ id + ' -E -s -i 10000 1e-10')
-                        
-                        result = read_result(id) 
-                        output_file.write(f"{VNF1};{VNF2};{VNF3};{VNF4};{perf};{result}\n")
+
+                        subprocess.check_output(
+                            '"C:\\Program Files (x86)\\TimeNET\\TimeNET\\EDSPN\\StatAnalysis\\scripts\\SOLVE.bat"' +
+                            ' ' +
+                            id +
+                            ' -E -s -i 10000 1e-10')
+
+                        result = read_result(id)
+                        output_file.write(
+                            f"{VNF1};{VNF2};{VNF3};{VNF4};{perf};{result}\n")
 
                 log_file = open(f"logs//{id}_log", "a+")
                 log_file.write(f"{counter}|{len(values)*2}\n")
-                log_file.close()    
-        
-        output_file.close()
-        shutil.rmtree(id+".dir")
-        os.remove("homogeneous\\data_"+id)
-    except:
-        df=pd.read_csv("cache/CACHE",sep=';')
-        df = df.drop(df[df['path']==id].index)
-        df.to_csv("cache/CACHE",index=False, sep=';')  
-        #fare roba al cache file, rimuovere l'id
+                log_file.close()
 
-    
+        output_file.close()
+        shutil.rmtree(id + ".dir")
+        os.remove("homogeneous\\data_" + id)
+    except BaseException:
+        df = pd.read_csv("cache/CACHE", sep=';')
+        df = df.drop(df[df['path'] == id].index)
+        df.to_csv("cache/CACHE", index=False, sep=';')
+        # fare roba al cache file, rimuovere l'id
+
+
 def compose_timenet_file(id):
-    filenames = ["homogeneous\\header", f"homogeneous\\data_{id}", "homogeneous\\footer"]
-    filename_timenet=f"{id}.TN"
+    filenames = [
+        "homogeneous\\header",
+        f"homogeneous\\data_{id}",
+        "homogeneous\\footer"]
+    filename_timenet = f"{id}.TN"
     with open(filename_timenet, 'w+') as file_timenet:
         for fname in filenames:
             with open(fname) as infile:
                 file_timenet.write(infile.read())
-   
+
+
 def read_result(id):
     result_file = open(f'{id}.dir\\{id}.RESULTS')
     result = float(result_file.readline().split("result = ")[1])
     result_file.close()
     return result
 
+
 def elaboration(configuration, id):
     for threshold in configuration['availability_target']:
         parallel_elab(configuration, id, threshold)
 
+
 def parallel_elab(configuration, id, threshold):
-    weights=configuration['weights']
-    costs=configuration['costs']
+    weights = configuration['weights']
+    costs = configuration['costs']
 
     filename = f"{id}.csv"
     cscf = {}
     hss = {}
     out_filename = f"results//{id}_{threshold}_results.csv"
-    
+
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
         line_count = 0
@@ -125,47 +152,66 @@ def parallel_elab(configuration, id, threshold):
                 if float(row[5].replace(',', '.')) >= threshold:
                     hss[conf] = float(row[5].replace(',', '.'))
 
-
     minCost = float('inf')
-    with open(out_filename, mode='w',newline='') as h_file:
+    with open(out_filename, mode='w', newline='') as h_file:
         h_writer = csv.writer(h_file, delimiter=';')
-        h_writer.writerow(['np1','np2','np3','np4','ns1','ns2','ns3','ns4','ni1','ni2','ni3','ni4','nh1','nh2','nh3','nh4','series','cost'])
+        h_writer.writerow(['np1',
+                           'np2',
+                           'np3',
+                           'np4',
+                           'ns1',
+                           'ns2',
+                           'ns3',
+                           'ns4',
+                           'ni1',
+                           'ni2',
+                           'ni3',
+                           'ni4',
+                           'nh1',
+                           'nh2',
+                           'nh3',
+                           'nh4',
+                           'series',
+                           'cost'])
         for h in hss:
-            costH = cost_calculator(h, costs=costs) 
+            costH = cost_calculator(h, costs=costs)
             parH = hss[h]
-            if costH > minCost*weights[0] or parH < threshold:
+            if costH > minCost * weights[0] or parH < threshold:
                 continue
             for i in cscf:
                 costIH = cost_calculator(i, costs=costs) + costH
                 parIH = cscf[i] * parH
-                if costIH > minCost*weights[1] or parIH < threshold:
+                if costIH > minCost * weights[1] or parIH < threshold:
                     continue
                 for p in cscf:
-                    costPIH = cost_calculator(p, costs=costs)+costIH
+                    costPIH = cost_calculator(p, costs=costs) + costIH
                     parPIH = cscf[p] * parIH
-                    if costPIH > minCost*weights[2] or parPIH < threshold:
+                    if costPIH > minCost * weights[2] or parPIH < threshold:
                         continue
                     for s in cscf:
                         cost = cost_calculator(s, costs=costs) + costPIH
                         par = cscf[s] * parPIH
-                        if cost > minCost*weights[3] or par < threshold:
+                        if cost > minCost * weights[3] or par < threshold:
                             continue
-                        h_writer.writerow(to_write([p,s,i,h], par, cost))
+                        h_writer.writerow(to_write([p, s, i, h], par, cost))
                         if cost < minCost:
                             minCost = cost
     os.remove(filename)
-    
-def cost_calculator(configuration, costs=[0.5,0.5,0.5]):
-    cost=0
-    for i in range(0,7,2):
+
+
+def cost_calculator(configuration, costs=[0.5, 0.5, 0.5]):
+    cost = 0
+    for i in range(0, 7, 2):
         if int(configuration[i]) != 0:
-            cost = cost + costs[1] + costs[2] + costs[0]*int(configuration[i])
+            cost = cost + costs[1] + costs[2] + \
+                costs[0] * int(configuration[i])
     return cost
 
-def to_write(configuration,result,cost):
-    to_write=[]
+
+def to_write(configuration, result, cost):
+    to_write = []
     for j in range(4):
-        for k in range(0,len(configuration[j]),2):
+        for k in range(0, len(configuration[j]), 2):
             to_write.append(configuration[j][k])
     to_write.append(str(result))
     to_write.append(str(cost))
--- source/validation.py
+++ source/validation.py
@@ -1,38 +1,50 @@
 class ValidationException(Exception):
     pass
 
-def validate_post_availability_input(data):
-    if 'Costs' in data and data['Costs'] is not None and len(data['Costs']) == 3:
-        try: 
-            costs =  [float(k) for k in data['Costs']]
-        except ValueError:
-            raise ValidationException('Costs should be an array of float. Try to use default [1,1,1]')
-    else:
-        raise ValidationException('Costs should be an array of size 3. Try to use default [1,1,1]')
-    
-    if 'AvailabilityTarget' in data and data['AvailabilityTarget'] is not None:
-        if type(data['AvailabilityTarget']) == str:
-            try:
-                availability_target = [data['AvailabilityTarget']] 
-            except:
-                raise ValidationException('AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
-        elif len(data['AvailabilityTarget']) > 0:
-            try: 
-                availability_target = [float(k) for k in data['AvailabilityTarget']]
-            except:
-                raise ValidationException('AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
-        else:
-            raise ValidationException('AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
-    else:
-        raise ValidationException('AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
-    
-    weights = [0.5,0.75,1,1.25] #pruning algorithm weights, could be configurable
 
-    configuration = {'costs':costs, 'weights':weights, 'availability_target':availability_target}
-    
+def validate_post_availability_input(data):
+    if 'Costs' in data and data['Costs'] is not None and len(
+            data['Costs']) == 3:
+        try:
+            costs = [float(k) for k in data['Costs']]
+        except ValueError:
+            raise ValidationException(
+                'Costs should be an array of float. Try to use default [1,1,1]')
+    else:
+        raise ValidationException(
+            'Costs should be an array of size 3. Try to use default [1,1,1]')
+
+    if 'AvailabilityTarget' in data and data['AvailabilityTarget'] is not None:
+        if isinstance(data['AvailabilityTarget'], str):
+            try:
+                availability_target = [data['AvailabilityTarget']]
+            except BaseException:
+                raise ValidationException(
+                    'AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
+        elif len(data['AvailabilityTarget']) > 0:
+            try:
+                availability_target = [
+                    float(k) for k in data['AvailabilityTarget']]
+            except BaseException:
+                raise ValidationException(
+                    'AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
+        else:
+            raise ValidationException(
+                'AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
+    else:
+        raise ValidationException(
+            'AvailabilityTarget should be an array of float. Try to use default [0.9999,0.99999,0.999999]')
+
+    # pruning algorithm weights, could be configurable
+    weights = [0.5, 0.75, 1, 1.25]
+
+    configuration = {'costs': costs, 'weights': weights,
+                     'availability_target': availability_target}
+
     parameters = validate_parameters(data)
 
     return configuration, parameters
+
 
 def validate_param(params, name):
     if name in params and params[name] is not None:
@@ -42,54 +54,68 @@ def validate_param(params, name):
             raise ValidationException(f'{name} should be a number')
     else:
         raise ValidationException(f'{name} is not present')
-    
+
+
 def validate_parameters(data):
-    params={}
+    params = {}
     if 'params' in data and data['params'] is not None:
         paramsJSON = data['params']
-        params['muHW'] = validate_param(paramsJSON,'muHW')
-        params['lamHW'] = validate_param(paramsJSON,'lamHW')
-        params['muVM'] = validate_param(paramsJSON,'muVM')
-        params['lamVM'] = validate_param(paramsJSON,'lamVM')
-        params['muVNF'] = validate_param(paramsJSON,'muVNF')
-        params['lamVNF'] = validate_param(paramsJSON,'lamVNF')
+        params['muHW'] = validate_param(paramsJSON, 'muHW')
+        params['lamHW'] = validate_param(paramsJSON, 'lamHW')
+        params['muVM'] = validate_param(paramsJSON, 'muVM')
+        params['lamVM'] = validate_param(paramsJSON, 'lamVM')
+        params['muVNF'] = validate_param(paramsJSON, 'muVNF')
+        params['lamVNF'] = validate_param(paramsJSON, 'lamVNF')
         if 'maxVNF' in paramsJSON and paramsJSON['maxVNF'] is not None:
             try:
-                params['maxVNF']=int(paramsJSON['maxVNF'])
+                params['maxVNF'] = int(paramsJSON['maxVNF'])
                 if params['maxVNF'] < 0 and params['maxVNF'] > 7:
-                    raise ValidationException('maxVNF should be an integer less then 7')
+                    raise ValidationException(
+                        'maxVNF should be an integer less then 7')
             except ValueError:
-                raise ValidationException('maxVNF should be an integer less than 7')
+                raise ValidationException(
+                    'maxVNF should be an integer less than 7')
         else:
-            raise ValidationException('maxVNF should be an integer less than 7')
+            raise ValidationException(
+                'maxVNF should be an integer less than 7')
         if 'maxNR' in paramsJSON and paramsJSON['maxNR'] is not None:
             try:
-                params['maxNR']=int(paramsJSON['maxNR'])
+                params['maxNR'] = int(paramsJSON['maxNR'])
                 if params['maxNR'] < 0 and params['maxNR'] > 4:
-                    raise ValidationException('maxNR should be an integer less than 5')
+                    raise ValidationException(
+                        'maxNR should be an integer less than 5')
             except ValueError:
-                raise ValidationException('maxNR should be an integer less than 5')
+                raise ValidationException(
+                    'maxNR should be an integer less than 5')
         else:
             raise ValidationException('maxNR should be an integer less than 5')
     else:
         raise ValidationException('parameter of simulation missing')
-    
+
     params['perf'] = 3
     return params
 
+
 def validate_get_availability_input(request):
     if request.args.get('id') is not None:
-        id = request.args.get('id') 
+        id = request.args.get('id')
     else:
         raise ValidationException('You need your id to access your data')
 
-    availability_target = request.args.get('AvailabilityTarget') if request.args.get('AvailabilityTarget') is not None else '0.9999'
-    maxSFC = request.args.get('maxSFC') if request.args.get('maxSFC') is not None else 10
-    
-    return {'id':id, 'maxSFC':maxSFC, 'availability_target':availability_target}
+    availability_target = request.args.get('AvailabilityTarget') if request.args.get(
+        'AvailabilityTarget') is not None else '0.9999'
+    maxSFC = request.args.get('maxSFC') if request.args.get(
+        'maxSFC') is not None else 10
+
+    return {
+        'id': id,
+        'maxSFC': maxSFC,
+        'availability_target': availability_target}
+
 
 def validate_post_performability_input(data):
     pass
+
 
 def validate_get_performability_input(request):
     pass
