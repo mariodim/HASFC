6289a8baca1c5f7974141fc3eceb2e6f547eb904
Giovanni.Galatro <giovanni.galatro@ibm.com>
2023-05-17 14:15:17 +0200

    Format autopep8 app.py

--- app.py
+++ app.py
@@ -7,36 +7,38 @@ from source import validation
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/availability', methods = ['GET','POST'])
+
+@app.route('/availability', methods=['GET', 'POST'])
 def availability():
     try:
-      if request.method == 'POST':
-        configuration, parameters = validation.validate_post_availability_input(request.get_json())
-        return controller.availability_post(configuration,parameters)
-      else:
-        parameters = validation.validate_get_availability_input(request)
-        return controller.availability_get(parameters)
+        if request.method == 'POST':
+            configuration, parameters = validation.validate_post_availability_input(
+                request.get_json())
+            return controller.availability_post(configuration, parameters)
+        else:
+            parameters = validation.validate_get_availability_input(request)
+            return controller.availability_get(parameters)
     except validation.ValidationException as e:
-      return Response(response=str(e), status=400,  mimetype="text/plain")
+        return Response(response=str(e), status=400, mimetype="text/plain")
     except Exception as e:
-       return Response(response=str(e), status=500,  mimetype="text/plain")
+        return Response(response=str(e), status=500, mimetype="text/plain")
 
 
-@app.route('/performability', methods = ['GET','POST'])
+@app.route('/performability', methods=['GET', 'POST'])
 def performability():
     try:
-      if request.method == 'POST':
-        configuration, parameters = validation.validate_post_performability_input(request.get_json())
-        return controller.performability_post(configuration, parameters)
-      else:
-        parameters = validation.validate_get_performability_input(request)
-        return controller.performability_get(parameters)
+        if request.method == 'POST':
+            configuration, parameters = validation.validate_post_performability_input(
+                request.get_json())
+            return controller.performability_post(configuration, parameters)
+        else:
+            parameters = validation.validate_get_performability_input(request)
+            return controller.performability_get(parameters)
     except validation.ValidationException as e:
-      return Response(response=str(e), status=400,  mimetype="text/plain")
+        return Response(response=str(e), status=400, mimetype="text/plain")
     except Exception as e:
-       return Response(response=str(e), status=500,  mimetype="text/plain")
-    
-  
+        return Response(response=str(e), status=500, mimetype="text/plain")
+
+
 if __name__ == '__main__':
-    serve(app, host = "0.0.0.0", port = 5002)
-    
+    serve(app, host="0.0.0.0", port=5002)
